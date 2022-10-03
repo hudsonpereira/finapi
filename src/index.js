d421dcbc0ca8c5e8187aa495a987e18c2ea74399
Hudson Pereira <hudson.byte@gmail.com>
2022-10-03 12:15:54 -0300

    check customer already exists

--- src/index.js
+++ src/index.js
@@ -15,12 +15,19 @@ const customers = []
  */
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body
-    const id = uuidV4()
+
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({
+            error: "Customer already exists!"
+        })
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidV4(),
         statements: []
     })
 
